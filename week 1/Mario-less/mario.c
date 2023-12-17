e64b86925b50f87325f8fa1dbd2b7537f7563ee4
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2023-12-17 12:11:59 -0300

    Update mario.c

--- week 1/Mario-less/mario.c
+++ week 1/Mario-less/mario.c
@@ -1,1 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int pyramidHeight;
+
+    do
+    {
+        pyramidHeight = get_int("How height is the Pyramid?\n");
+    }
+    while (pyramidHeight < 1 || pyramidHeight > 8);
+
+    for (int pyramidWidth = 1; pyramidWidth <= pyramidHeight; pyramidWidth++)
+    {
+        for (int hash = 1; hash <= pyramidHeight; hash++)
+        {
+            if (hash <= pyramidHeight - pyramidWidth)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+}
